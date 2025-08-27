746c1e8c3da320c3aac581454c20523f922132e5
rupeshpatil <rupeshpatil88927@gmail.com>
2025-08-27 23:18:10 +0530

    spaceman 3d model setup

---
+++ 3d-spaceman/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+ 
+  return (
+
+    <div className='relative w-full h-[100vh]'>
+
+      <div className='flex flex-col gap-1 text-2xl font-bold items-center justify-center absolute inset-0'>
+        <h1>Spaceman 3d Model</h1>
+      </div>
+
+    </div>
+
+  )
+}
+
+export default App
